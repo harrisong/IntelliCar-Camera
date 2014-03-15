a28964fd2b68077a56831add0477aaf6d40039b0
harrisong <tplawyer@gmail.com>
2014-03-16 01:24:29 +0800

    New accel params. (Not tested yet)

--- src/balance_gyro.cpp
+++ src/balance_gyro.cpp
@@ -6,28 +6,40 @@
  */
 #include <balance_gyro.h>
 
-balance_gyro::balance_gyro(ADCn_Ch_e p1, ADCn_Ch_e p2, ADCn_Ch_e p3, int16 sp){
+balance_gyro::balance_gyro(ADCn_Ch_e p1, ADCn_Ch_e p2, ADCn_Ch_e p3, ADCn_Ch_e p4, int16 sp){
 	raw_gyro_port = p1;
 	raw_angle_port = p2;
-	raw_accel_port = p3;
+	raw_z_port = p3;
+	raw_x_port = p4;
 	adc_init(raw_gyro_port);
 	adc_init(raw_angle_port);
-	adc_init(raw_accel_port);
+	adc_init(raw_z_port);
+	adc_init(raw_x_port);
 
 	raw_setpoint = sp;
 	raw_gyro = 0;
+	
+	Vmax = 3.3;
+	Adc16max = 65535;
+	Gyrozero = 1.23;
+	Gyroscale = 0.00067;
+	Accelzero = 1.65;
+	Accelscale = 0.4785;
+	
 	refresh();
 }
 
 void balance_gyro::refresh(){
 	totalsample++;
-	//raw_gyro = ((int16) adc_once(raw_gyro_port, ADC_16bit) + raw_gyro)/totalsample;
-	raw_gyro = (adc_once(raw_gyro_port, ADC_16bit) * 3.3 / 65535 - 1.23) / 0.00067;
-
 	raw_angle = (int16) adc_once(raw_angle_port, ADC_16bit);
 	raw_offset = raw_angle - raw_setpoint;
-
-	raw_z =  (int16)adc_once(raw_accel_port, ADC_16bit);
+	
+	//raw_gyro = ((int16) adc_once(raw_gyro_port, ADC_16bit) + raw_gyro)/totalsample;
+	raw_gyro = (adc_once(raw_gyro_port, ADC_16bit) * Vmax / Adc16max - Gyrozero) / Gyroscale;
+	
+	Rx =  (adc_once(raw_x_port, ADC_16bit) * Vmax / Adc16max - Accelzero) / Accelscale;
+	Rz =  (adc_once(raw_z_port, ADC_16bit) * Vmax / Adc16max - Accelzero) / Accelscale;
+	accel = sqrt(pow(Rx,2) + pow(Rz,2));
 }
 
 float balance_gyro::get_raw_gyro(){
@@ -43,6 +55,6 @@ int16 balance_gyro::get_offset(){
 }
 
 int16 balance_gyro::get_accel(){
-	return raw_z;
+	return accel;
 }
 
