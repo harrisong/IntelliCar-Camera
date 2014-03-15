c8deab0d9f5df6d2a43c1a202a5e0b8a96f015be
harrisong <tplawyer@gmail.com>
2014-03-16 01:28:51 +0800

    updated prototypes

--- inc/balance_gyro.h
+++ inc/balance_gyro.h
@@ -16,21 +16,28 @@
 class balance_gyro{
 private:
 	ADCn_Ch_e raw_gyro_port, raw_angle_port, raw_accel_port;
-	int16 raw_gyro;
+	float raw_gyro;
 	int16 raw_angle;
-	int16 raw_z;
+	float Rz, Rx, accel;
 	int16 raw_setpoint;
 	int16 raw_offset;
 	int totalsample;
+	
+	float Vmax;
+	int16 Adc16max;
+	float Gyrozero;
+	float Gyroscale;
+	float Accelzero;
+	float Accelscale;
 
 
 public:
-	balance_gyro(ADCn_Ch_e, ADCn_Ch_e, ADCn_Ch_e, int16);
+	balance_gyro(ADCn_Ch_e, ADCn_Ch_e, ADCn_Ch_e, ADCn_Ch_e, int16);
 	void refresh();
-	int16 get_raw_gyro();
+	float get_raw_gyro();
 	int16 get_raw_angle();
 	int16 get_offset();
-	int16 get_accel();
+	float get_accel();
 
 
 };
