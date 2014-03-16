59ea9f3048034209ce6a474bf90360ceecf3fd5a
Harrison Ng <tplawyer@gmail.com>
2014-03-16 18:38:43 +0800

    Redefine ports in custom def.

--- inc/vars.h
+++ inc/vars.h
@@ -8,12 +8,46 @@
 #ifndef VARS_H_
 #define VARS_H_
 
+//speed pid vars//
 #define kp 1950
 #define kd 0
 #define ki 0
 
+//Encoders Ports Definition//
+#ifdef FTM1_QDPHA
+#undef FTM1_QDPHA
+#define FTM1_QDPHA PTB0
+#endif
 
+#ifdef FTM1_QDPHB
+#undef FTM1_QDPHB
+#define FTM1_QDPHB PTB1
+#endif
 
+#ifdef FTM2_QDPHA
+#undef FTM2_QDPHA
+#define FTM2_QDPHA PTB18
+#endif
 
+#ifdef FTM2_QDPHB
+#undef FTM2_QDPHB
+#define FTM2_QDPHB PTB19
+#endif
+
+#ifdef FTM2_QDPHB
+#undef FTM2_QDPHB
+#define FTM2_QDPHB PTB19
+#endif
+
+//Motors Ports Definition//
+#ifdef FTM0_CH0
+#undef FTM0_CH0
+#define FTM0_CH0 PTC1
+#endif
+
+#ifdef FTM0_CH1
+#undef FTM0_CH1
+#define FTM0_CH1 PTC2
+#endif
 
 #endif /* VARS_H_ */
