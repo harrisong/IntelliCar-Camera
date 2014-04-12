d02cfca638314fb8116835273f5467eba0c08a65
Harrison Ng <tplawyer@gmail.com>
2014-04-12 13:27:54 +0800

    Stable PD

--- inc/vars.h
+++ inc/vars.h
@@ -9,8 +9,8 @@
 #define VARS_H_
 
 //balance pid vars//
-#define balance_kp 3.78f
-#define balance_kd 0.06f
+#define balance_kp 6.0f
+#define balance_kd 1.40f
 #define balance_ki 0.0f
 
 #define SETPOINT 30780
@@ -18,8 +18,8 @@
 #define DEADZONEHIGHER 39000
 
 //speed pid vars//
-#define speed_kp 4.0f
-#define speed_kd 2.0f
+#define speed_kp 5.0f
+#define speed_kd 0.0f
 #define speed_ki 0.0f
 #define SPEEDSETPOINT 500
 
