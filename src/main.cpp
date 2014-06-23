bfe60146382d9e463a8b2b5156444e851acaa314
Harrison Ng <tplawyer@gmail.com>
2014-06-23 22:52:25 +0800

    Remove garbage

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,9 @@
 #include "camera/camera_app.h"
 
-#include <libsc/com/motor.h>
-using namespace libsc;
-
 int main()
 {
 	camera::CameraApp app;
     app.Run();
-
-	/*Motor m1(0), m2(1);
-	m1.SetClockwise(true);
-	m2.SetClockwise(true);
-
-	while(true){
-		m1.SetPower(5000);
-		m2.SetPower(5000);
-	}*/
 
     return 0;
 }
