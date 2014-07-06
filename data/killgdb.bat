b0b29fb342dd1ce8f8f35b8a005a442ce0a6fc44
Harrison Ng <tplawyer@gmail.com>
2014-07-06 14:05:21 +0800

    .gitignore is now working

--- data/killgdb.bat
+++
@@ -1,5 +0,0 @@
-C:\Windows\System32\taskkill.exe /F /IM JLinkGDBServer.exe
-start "" "C:\Program Files (x86)\SEGGER\JLinkARM_V480a\JLinkGDBServer.exe" -select USB -device MK60DN512xxx10 -if JTAG -speed 1000
-REM delay 1.5s
-ping 192.0.2.2 -n 1 -w 1500 > nul
-"C:\Users\FaiFai\Desktop\Programming\Eclipse SmartCar\gcc-arm-none-eabi-4_8-2013q4-20131204-win32\bin\arm-none-eabi-gdb.exe" %*
